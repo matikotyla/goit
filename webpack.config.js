c120278d7fd82f67400ce9682a893617161637b4
Mateusz Kotyla <mati.kotyla@gmail.com>
2022-11-03 22:17:01 +0100

    Change the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,52 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
-module.exports = {
-  entry: {
-    index: "./src/index.js",
-  },
-  devServer: {
-    port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
-        },
-      },
+module.exports = function () {
+  return {
+    mode: "production",
+    entry: ["./src/app.js"],
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack starter project",
+        template: path.resolve("./src/index.html"),
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+      new HtmlWebpackInlineSVGPlugin({
+        inlineAll: true,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      inject: true,
-      chunks: ["index"],
-      filename: "index.html",
-    }),
-    new HtmlWebpackInlineSVGPlugin({
-      inlineAll: true,
-    }),
-  ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: "html-loader",
+          },
+        },
+      ],
+    },
+  };
 };
